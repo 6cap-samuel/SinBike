4a71e63cf34f48c41455eae859872cdcb694d579
Theodora <theodorajean@gmail.com>
2019-05-13 14:28:08 +0800

    Updated
    Signed-off-by: Theodora <theodorajean@gmail.com>

--- app/src/main/java/com/example/sinbike/Activities/AboutUs.java
+++ app/src/main/java/com/example/sinbike/Activities/AboutUs.java
@@ -1,8 +1,10 @@
 package com.example.sinbike.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import com.example.sinbike.R;
 
@@ -12,5 +14,16 @@ public class AboutUs extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
+
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setTitle("Forget Password");
+        toolbar.setNavigationOnClickListener(v -> {
+            Intent intent = new Intent(AboutUs.this, ManageDashboardActivity.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
